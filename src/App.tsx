3afc8be6c84e63f25b49f99ed28709b8aa30dcc0
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-06-06 06:38:54 -0300

    Criando componente rota ProductDetails

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
 
-import './App.css';
-import computerImg from '../src/assets/computer.png';
-import HeaderClient from './components/HeaderClient';
-import ButtonPrimary from './components/ButtonPrimary';
-import ButtonInverse from './components/ButtonInverse';
-import ProductDetailsCard from './components/ProductDetailsCard';
-
+import ProductDetails from './routes/ProductDetails';
 
 export default function App() {
 
   return (
-    <>
-      <HeaderClient />
-      <main>
-        <section id="product-details-section" className="dsc-container">
-          <ProductDetailsCard />
-            <div className="dsc-btn-page-container">
-                <ButtonPrimary />
-                <ButtonInverse />
-            </div>
-        </section>
-      </main>
-    </>  
+    <ProductDetails />
   );
 }
