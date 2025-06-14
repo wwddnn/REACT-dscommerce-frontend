b35454f3a3dc0181ab22403c0efc8ddaa7676c6e
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-06-14 14:18:23 -0300

    Exercicio de Props Card do Catalog

--- src/components/CatalogCard/index.tsx
+++ src/components/CatalogCard/index.tsx
@@ -1,16 +1,22 @@
 import './styles.css';
-import computerImg from '../../assets/computer.png';
+import type { ProductDTO } from '../../models/product';
 
-export default function CatalogCard() {
+/* fazer a props pra receber o product como argumento la embaixo*/
+type Props = {
+  product: ProductDTO;
+}
+
+
+export default function CatalogCard({ product }: Props) {
   
     return (
     <div className="dsc-card">
       <div className="dsc-catalog-card-top dsc-line-bottom">
-        <img src={computerImg} alt="Computer" />
+        <img src={product.imgUrl} alt={product.name} />
       </div>
       <div className="dsc-catalog-card-bottom">
-        <h3>R$ 5000,00</h3>
-        <h4>Computador Gamer XT</h4>
+        <h3>R$ {product.price.toFixed(2)}</h3>
+        <h4>{product.name}</h4>
       </div>
     </div>
   );
--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -3,6 +3,31 @@ import HeaderClient from "../../components/HeaderClient";
 import Searchbar from "../../components/Searchbar";
 import CatalogCard from "../../components/CatalogCard";
 import ButtonNextPage from "../../components/ButtonNextPage";
+import type { ProductDTO } from "../../models/product";
+
+const product: ProductDTO = {
+  id: 2,
+  name: "Smart TV",
+  description: "Essa TV é muito boa",
+  imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/refs/heads/master/backend/img/2-big.jpg",
+  price: 2500.99,
+  categories: [
+    {
+    id: 2,
+    name: "Eletrônicos"
+    },
+    {
+    id: 3,
+    name: "Computadores"
+    },
+    {
+    id: 4,
+    name: "Importados"
+    }
+  ]
+}
+
+
 
 export default function Catalog() {
   
@@ -14,17 +39,17 @@ export default function Catalog() {
           <Searchbar />
 
           <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
+            <CatalogCard product= {product} />
+            <CatalogCard product= {product} />
+            <CatalogCard product= {product} />
+            <CatalogCard product= {product} />
+            <CatalogCard product= {product} />
+            <CatalogCard product= {product} />
+            <CatalogCard product= {product} />
+            <CatalogCard product= {product} />
+            <CatalogCard product= {product} />
+            <CatalogCard product= {product} />
+            <CatalogCard product= {product} />
           </div>  
 
           <ButtonNextPage />
