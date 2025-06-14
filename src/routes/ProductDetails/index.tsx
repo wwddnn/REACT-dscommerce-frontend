fa6128cec49ba196a8dfcfc9c639d59ca5283baa
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-06-14 13:54:13 -0300

    Exercicio de Props botoes

--- src/routes/ProductDetails/index.tsx
+++ src/routes/ProductDetails/index.tsx
@@ -38,8 +38,8 @@ export default function ProductDetails() {
                 <section id="product-details-section" className="dsc-container">
                   <ProductDetailsCard product= {product}/>
                     <div className="dsc-btn-page-container">
-                        <ButtonPrimary />
-                        <ButtonInverse />
+                        <ButtonPrimary text= "Comprar" />
+                        <ButtonInverse text= "Início" />
                     </div>
                 </section>
               </main>
