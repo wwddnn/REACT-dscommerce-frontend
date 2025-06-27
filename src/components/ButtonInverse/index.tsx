86086b0ace67677af0288beb9d276cc5911f9b72
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-06-27 15:50:07 -0300

    Links catalog product details

--- src/components/ButtonInverse/index.tsx
+++ src/components/ButtonInverse/index.tsx
@@ -1,13 +1,13 @@
-import './styles.css';
+import "./styles.css";
 
 type Props = {
-    text: string;
-}
+  text: string;
+};
 
 export default function ButtonInverse({ text }: Props) {
-    return(
-        <div className="dsc-btn dsc-btn-white">
-            { text }
-        </div>
-    );
+  return (
+      <div className="dsc-btn dsc-btn-white">
+        {text}
+      </div>
+  );
 }
--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -4,6 +4,7 @@ import ButtonPrimary from '../../../components/ButtonPrimary';
 import ProductDetailsCard from '../../../components/ProductDetailsCard';
 import * as productService from '../../../services/product-service';
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function ProductDetails() {
 
@@ -20,7 +21,9 @@ export default function ProductDetails() {
         }
         <div className="dsc-btn-page-container">
           <ButtonPrimary text="Comprar" />
-          <ButtonInverse text="Início" />
+          <Link to="/">
+            <ButtonInverse text="Início" />
+          </Link>
         </div>
       </section>
     </main>
