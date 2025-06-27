d86edf6bdf0c75638878f4a8832fe6e848bcef59
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-06-27 14:47:26 -0300

    Adding React Router DOM

---
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -0,0 +1,54 @@
+import "./styles.css";
+import Searchbar from "../../../components/Searchbar";
+import CatalogCard from "../../../components/CatalogCard";
+import ButtonNextPage from "../../../components/ButtonNextPage";
+import type { ProductDTO } from "../../../models/product";
+
+const product: ProductDTO = {
+  id: 2,
+  name: "Smart TV",
+  description: "Essa TV é muito boa",
+  imgUrl:
+    "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/refs/heads/master/backend/img/2-big.jpg",
+  price: 2500.99,
+  categories: [
+    {
+      id: 2,
+      name: "Eletrônicos",
+    },
+    {
+      id: 3,
+      name: "Computadores",
+    },
+    {
+      id: 4,
+      name: "Importados",
+    },
+  ],
+};
+
+export default function Catalog() {
+  return (
+    <main>
+      <section id="catalog-section" className="dsc-container">
+        <Searchbar />
+
+        <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+          <CatalogCard product={product} />
+        </div>
+
+        <ButtonNextPage />
+      </section>
+    </main>
+  );
+}
