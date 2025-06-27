5fe0a52b123decb9fd26936d87350e3192f9c30c
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-06-27 15:40:55 -0300

    Producto dinamico com parametro de rota

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -1,37 +1,23 @@
-import "./styles.css";
-import ButtonInverse from "../../../components/ButtonInverse";
-import ButtonPrimary from "../../../components/ButtonPrimary";
-import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import type { ProductDTO } from "../../../models/product";
-
-const product: ProductDTO = {
-  id: 2,
-  name: "Smart TV",
-  description: "Essa TV é muito boa",
-  imgUrl:
-    "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/refs/heads/master/backend/img/2-big.jpg",
-  price: 2500.99,
-  categories: [
-    {
-      id: 2,
-      name: "Eletrônicos",
-    },
-    {
-      id: 3,
-      name: "Computadores",
-    },
-    {
-      id: 4,
-      name: "Importados",
-    },
-  ],
-};
+import './styles.css';
+import ButtonInverse from '../../../components/ButtonInverse';
+import ButtonPrimary from '../../../components/ButtonPrimary';
+import ProductDetailsCard from '../../../components/ProductDetailsCard';
+import * as productService from '../../../services/product-service';
+import { useParams } from 'react-router-dom';
 
 export default function ProductDetails() {
+
+  const params = useParams();
+
+  const product = productService.findById(Number(params.productId));
+
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
-        <ProductDetailsCard product={product} />
+        {
+          product &&
+          <ProductDetailsCard product={product} />
+        }
         <div className="dsc-btn-page-container">
           <ButtonPrimary text="Comprar" />
           <ButtonInverse text="Início" />
